89eca7bb0fa57c07ae8da59298687cee72642162
vijay-0309 <vijaynm.xworkz@gmail.com>
2021-10-05 23:11:06 +0530

    Added Run time polymorphism

---
+++ Engineering/Engineering.java
@@ -0,0 +1,10 @@
+class Engineering{
+
+
+String name;
+
+public static void problemSolving(String name){
+System.out.println("Problem Solved by engineer");
+}
+
+}
---
+++ Engineering/MechanicalEngineering.java
@@ -0,0 +1,11 @@
+class MechanicalEngineering extends Engineering{
+
+public MechanicalEngineering(){
+System.out.println(this.getClass().getSimpleName()+" object is created");
+}
+
+public static void problemSolving(String name){
+System.out.println(" Engineering class is created");
+}
+
+}
